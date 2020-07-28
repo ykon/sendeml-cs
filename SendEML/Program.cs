3da5d0de546c47ecc8f0015f49fee1a054dbdb01
Yuki Ono <ykon0x1@gmail.com>
2020-07-29 00:31:24 +0900

    first commit

---
+++ SendEML/Program.cs
@@ -0,0 +1,336 @@
+﻿/**
+ * Copyright (c) Yuki Ono.
+ * Licensed under the MIT License.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SendEML {
+    using SendCmd = Func<String, String>;
+    public class Program {
+        const decimal VERSION = 1.0m;
+
+        //const char CR = '\r';
+        const char LF = '\n';
+        const string CRLF = "\r\n";
+
+        static readonly Regex DATE_REGEX = new Regex(@"^Date: [\S ]+", RegexOptions.Compiled);
+        static readonly Regex MESSAGE_ID_REGEX = new Regex(@"^Message-ID: [\S]+", RegexOptions.Compiled);
+
+        public static bool IsDateLine(string line) {
+            return DATE_REGEX.IsMatch(line);
+        }
+
+        public static string MakeNowDateLine() {
+            var us = new CultureInfo("en-US");
+            var now = DateTimeOffset.Now;
+            var offset = now.ToString("zzz", us).Replace(":", "");
+            return "Date: " + now.ToString("ddd, dd MMM yyyy HH:mm:ss ", us) + offset;
+        }
+
+        public static bool IsMessageIdLine(string line) {
+            return MESSAGE_ID_REGEX.IsMatch(line);
+        }
+
+        static readonly Random random = new Random();
+        public static string MakeRandomMessageIdLine(int length = 62) {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            var rand_str = new string(Enumerable.Repeat(chars, length).Select(s => s[random.Next(s.Length)]).ToArray());
+            return $"Message-ID: <{rand_str}>";
+        }
+
+        public static int FindLfIndex(byte[] file_buf, int offset) {
+            return Array.IndexOf(file_buf, (byte)LF, offset);
+        }
+
+        public static List<int> FindAllLfIndices(byte[] file_buf) {
+            var indices = new List<int>();
+            var i = 0;
+            while (true) {
+                i = FindLfIndex(file_buf, i);
+                if (i == -1)
+                    return indices;
+
+                indices.Add(i);
+                i += 1;
+            }
+        }
+
+        public static byte[] CopyNew(byte[] src_buf, int offset, int count) {
+            var dest_buf = new byte[count];
+            Buffer.BlockCopy(src_buf, offset, dest_buf, 0, dest_buf.Length);
+            return dest_buf;
+        }
+
+        public static List<byte[]> GetRawLines(byte[] file_buf) {
+            var offset = 0;
+            return FindAllLfIndices(file_buf).Append(file_buf.Length - 1).Select(i => {
+                var line = CopyNew(file_buf, offset, i - offset + 1);
+                offset = i + 1;
+                return line;
+            }).ToList();
+        }
+
+        public static bool IsFirstD(byte[] line) {
+            return line.FirstOrDefault() == (byte)'D';
+        }
+
+        public static bool IsFirstM(byte[] line) {
+            return line.FirstOrDefault() == (byte)'M';
+        }
+
+        public static int FindDateLineIndex(List<byte[]> lines) {
+            return lines.FindIndex(l => IsFirstD(l) && IsDateLine(Encoding.UTF8.GetString(l)));
+        }
+
+        public static int FindMessageIdLineIndex(List<byte[]> lines) {
+            return lines.FindIndex(l => IsFirstM(l) && IsMessageIdLine(Encoding.UTF8.GetString(l)));
+        }
+
+        public static List<byte[]> ReplaceRawLines(List<byte[]> lines, bool update_date, bool update_message_id) {
+            if (!update_date && !update_message_id)
+                return lines;
+
+            var reps_lines = new List<byte[]>(lines);
+
+            if (update_date) {
+                var date_idx = FindDateLineIndex(reps_lines);
+                if (date_idx != -1)
+                    reps_lines[date_idx] = Encoding.UTF8.GetBytes(MakeNowDateLine());
+            }
+            if (update_message_id) {
+                var mid_idx = FindMessageIdLineIndex(reps_lines);
+                if (mid_idx != -1)
+                    reps_lines[mid_idx] = Encoding.UTF8.GetBytes(MakeRandomMessageIdLine());
+            }
+
+            return reps_lines;
+        }
+
+        public static byte[] ConcatRawLines(List<byte[]> lines) {
+            var buf = new byte[lines.Sum(l => l.Length)];
+            var offset = 0;
+            foreach (var l in lines) {
+                Buffer.BlockCopy(l, 0, buf, offset, l.Length);
+                offset += l.Length;
+            }
+            return buf;
+        }
+
+        public static byte[] ReplaceRawBytes(byte[] file_buf, bool update_date, bool update_message_id) {
+            return ConcatRawLines(ReplaceRawLines(GetRawLines(file_buf), update_date, update_message_id));
+        }
+
+        static volatile bool useParallel = false;
+
+        public static String GetCurrentIdPrefix() {
+            return useParallel ? $"id: {Task.CurrentId}, " : "";
+        }
+
+        public static void SendRawBytes(Stream stream, string file, bool update_date, bool update_message_id) {
+            var path = Path.GetFullPath(file);
+            Console.WriteLine(GetCurrentIdPrefix() + $"send: {path}");
+
+            var buf = ReplaceRawBytes(File.ReadAllBytes(path), update_date, update_message_id);
+            stream.Write(buf, 0, buf.Length);
+            stream.Flush();
+        }
+
+        public class Settings {
+            public string SmtpHost { get; set; }
+            public int SmtpPort { get; set; }
+            public string FromAddress { get; set; }
+            public List<string> ToAddress { get; set; }
+            public List<string> EmlFile { get; set; }
+            public bool UpdateDate { get; set; }
+            public bool UpdateMessageId { get; set; }
+            public bool UseParallel { get; set; }
+        }
+
+        public static Settings GetSettings(string file) {
+            var path = Path.GetFullPath(file);
+            var options = new JsonSerializerOptions {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                ReadCommentHandling = JsonCommentHandling.Skip
+            };
+
+            return JsonSerializer.Deserialize<Settings>(File.ReadAllText(path), options);
+        }
+
+        static readonly Regex RECV_REGEX = new Regex(@"^\d{3} .+", RegexOptions.Compiled);
+
+        public static bool IsSuccess(string line) {
+            return (new[] {'2', '3'}).Contains(line.FirstOrDefault());
+        }
+
+        public static string RecvLine(StreamReader reader) {
+            while (true) {
+                var line = reader.ReadLine()?.Trim() ?? throw new IOException("Connection closed by foreign host.");
+                Console.WriteLine(GetCurrentIdPrefix() + $"recv: {line}");
+
+                if (RECV_REGEX.IsMatch(line)) {
+                    if (!IsSuccess(line))
+                        throw new IOException(line);
+
+                    return line;
+                }
+            }
+        }
+
+        public static void SendLine(StreamWriter writer, string cmd) {
+            Console.WriteLine(GetCurrentIdPrefix() + "send: " + ((cmd == $"{CRLF}.") ? "<CRLF>." : cmd));
+
+            writer.WriteLine(cmd);
+            writer.Flush();
+        }
+
+        public static SendCmd MakeSendCmd(StreamReader reader, StreamWriter writer) {
+            return cmd => {
+                SendLine(writer, cmd);
+                return RecvLine(reader);
+            };
+        }
+
+        public static void SendHello(SendCmd send) {
+            send("EHLO localhost");
+        }
+
+        public static void SendFrom(SendCmd send, string from_addr) {
+            send($"MAIL FROM: <{from_addr}>");
+        }
+
+        public static void SendRcptTo(SendCmd send, List<string> to_addrs) {
+            foreach (var addr in to_addrs)
+                send($"RCPT TO: <{addr}>");
+        }
+
+        public static void SendData(SendCmd send) {
+            send("DATA");
+        }
+
+        public static void SendCrLfDot(SendCmd send) {
+            send($"{CRLF}.");
+        }
+
+        public static void SendQuit(SendCmd send) {
+            send("QUIT");
+        }
+
+        public static void SendRset(SendCmd send) {
+            send("RSET");
+        }
+
+        public static void SendMessages(Settings settings, List<string> eml_files) {
+            using var socket = new TcpClient(settings.SmtpHost, settings.SmtpPort);
+            var stream = socket.GetStream();
+            stream.ReadTimeout = 1000;
+
+            var reader = new StreamReader(stream);
+            var writer = new StreamWriter(stream) {
+                NewLine = CRLF
+            };
+            var send = MakeSendCmd(reader, writer);
+            RecvLine(reader);
+            SendHello(send);
+
+            var mail_sent = false;
+            foreach (var file in eml_files) {
+                if (!File.Exists(file)) {
+                    Console.WriteLine($"{file}: EML file does not exists");
+                    continue;
+                }
+
+                if (mail_sent) {
+                    Console.WriteLine("---");
+                    SendRset(send);
+                }
+
+                SendFrom(send, settings.FromAddress);
+                SendRcptTo(send, settings.ToAddress);
+                SendData(send);
+                SendRawBytes(stream, file, settings.UpdateDate, settings.UpdateMessageId);
+                SendCrLfDot(send);
+                mail_sent = true;
+            }
+
+            SendQuit(send);
+        }
+
+        public static void SendOneMessage(Settings settings, string file) {
+            SendMessages(settings, new List<string> { file });
+        }
+
+        public static string MakeJsonSample() {
+            return @"{
+    ""smtpHost"": ""172.16.3.151"",
+    ""smtpPort"": 25,
+    ""fromAddress"": ""a001@ah62.example.jp"",
+    ""toAddress"": [
+        ""a001@ah62.example.jp"",
+        ""a002@ah62.example.jp"",
+        ""a003@ah62.example.jp""
+    ],
+    ""emlFile"": [
+        ""test1.eml"",
+        ""test2.eml"",
+        ""test3.eml""
+    ],
+    ""updateDate"": true,
+    ""updateMessageId"": true,
+    ""useParallel"": false
+}";
+        }
+
+        public static void WriteUsage() {
+            Console.WriteLine("Usage: {self} json_file ...");
+            Console.WriteLine("---");
+
+            Console.WriteLine("json_file sample:");
+            Console.WriteLine(MakeJsonSample());
+        }
+
+        static void WriteVersion() {
+            Console.WriteLine($"SendEML / Version: {VERSION}");
+        }
+
+        static void Main(string[] args) {
+            if (!args.Any()) {
+                WriteUsage();
+                return;
+            }
+
+            if (args[0] == "--version") {
+                WriteVersion();
+                return;
+            }
+
+            foreach (var json_file in args) {
+                if (!File.Exists(json_file)) {
+                    Console.WriteLine($"{json_file}: Json file does not exists");
+                    continue;
+                }
+
+                try {
+                    var settings = GetSettings(json_file);
+                    if (settings.UseParallel) {
+                        useParallel = true;
+                        settings.EmlFile.AsParallel().ForAll(f => SendOneMessage(settings, f));
+                    } else {
+                        SendMessages(settings, settings.EmlFile);
+                    }
+                } catch (Exception e) {
+                    Console.WriteLine($"{json_file}: {e.Message}");
+                }
+            }
+        }
+    }
+}
---
+++ SendEMLTests/ProgramTests.cs
@@ -0,0 +1,343 @@
+﻿/**
+ * Copyright (c) Yuki Ono.
+ * Licensed under the MIT License.
+ */
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.IO;
+
+namespace SendEML.Tests {
+    using SendCmd = Func<String, String>;
+
+    [TestClass()]
+    public class ProgramTests {
+        [TestMethod()]
+        public void IsDateLineTest() {
+            Assert.IsTrue(Program.IsDateLine("Date: xxx"));
+            Assert.IsFalse(Program.IsDateLine("xxx: Date"));
+            Assert.IsFalse(Program.IsDateLine("X-Date: xxx"));
+        }
+
+        [TestMethod()]
+        public void MakeNowDateLineTest() {
+            var line = Program.MakeNowDateLine();
+            Assert.IsTrue(line.StartsWith("Date: "));
+            Assert.IsTrue(line.Length <= 76);
+        }
+
+        [TestMethod()]
+        public void IsMessageIdLineTest() {
+            Assert.IsTrue(Program.IsMessageIdLine("Message-ID: xxx"));
+            Assert.IsFalse(Program.IsMessageIdLine("xxx: Message-ID"));
+            Assert.IsFalse(Program.IsMessageIdLine("X-Message-ID: xxx"));
+        }
+
+        [TestMethod()]
+        public void MakeRandomMessageIdLineTest() {
+            var line = Program.MakeRandomMessageIdLine();
+            Assert.IsTrue(line.StartsWith("Message-ID: "));
+            Assert.IsTrue(line.Length <= 76);
+        }
+
+        string MakeSimpleMail() {
+            return @"From: a001 <a001@ah62.example.jp>
+Subject: test
+To: a002@ah62.example.jp
+Message-ID: <b0e564a5-4f70-761a-e103-70119d1bcb32@ah62.example.jp>
+Date: Sun, 26 Jul 2020 22:01:37 +0900
+User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:78.0) Gecko/20100101
+ Thunderbird/78.0.1
+MIME-Version: 1.0
+Content-Type: text/plain; charset=utf-8; format=flowed
+Content-Transfer-Encoding: 7bit
+Content-Language: en-US
+
+test";
+        }
+
+        [TestMethod()]
+        public void FindLfIndexTest() {
+            var mail = Encoding.UTF8.GetBytes(MakeSimpleMail());
+            Assert.AreEqual(34, Program.FindLfIndex(mail, 0));
+            Assert.AreEqual(49, Program.FindLfIndex(mail, 35));
+            Assert.AreEqual(75, Program.FindLfIndex(mail, 59));
+        }
+
+        [TestMethod()]
+        public void FindAllLfIndicesTest() {
+            var mail = Encoding.UTF8.GetBytes(MakeSimpleMail());
+            var indices = Program.FindAllLfIndices(mail);
+
+            Assert.AreEqual(34, indices[0]);
+            Assert.AreEqual(49, indices[1]);
+            Assert.AreEqual(75, indices[2]);
+
+            Assert.AreEqual(390, indices[indices.Count - 3]);
+            Assert.AreEqual(415, indices[indices.Count - 2]);
+            Assert.AreEqual(417, indices[indices.Count - 1]);
+        }
+
+        [TestMethod()]
+        public void CopyNewTest() {
+            var mail = Encoding.UTF8.GetBytes(MakeSimpleMail());
+
+            var buf = Program.CopyNew(mail, 0, 10);
+            Assert.AreEqual(10, buf.Length);
+            Assert.IsTrue(mail.Take(10).SequenceEqual(buf));
+
+            var buf2 = Program.CopyNew(mail, 10, 20);
+            Assert.AreEqual(20, buf2.Length);
+            Assert.IsTrue(mail.Skip(10).Take(20).SequenceEqual(buf2));
+
+            buf[0] = 0;
+            Assert.AreEqual(0, buf[0]);
+            Assert.AreNotEqual(0, mail[0]);
+        }
+
+        [TestMethod()]
+        public void GetRawLinesTest() {
+            var mail = Encoding.UTF8.GetBytes(MakeSimpleMail());
+            var lines = Program.GetRawLines(mail);
+
+            Assert.AreEqual(13, lines.Count);
+            Assert.AreEqual("From: a001 <a001@ah62.example.jp>\r\n", Encoding.UTF8.GetString(lines[0]));
+            Assert.AreEqual("Subject: test\r\n", Encoding.UTF8.GetString(lines[1]));
+            Assert.AreEqual("To: a002@ah62.example.jp\r\n", Encoding.UTF8.GetString(lines[2]));
+
+            Assert.AreEqual("Content-Language: en-US\r\n", Encoding.UTF8.GetString(lines[lines.Count - 3]));
+            Assert.AreEqual("\r\n", Encoding.UTF8.GetString(lines[lines.Count - 2]));
+            Assert.AreEqual("test", Encoding.UTF8.GetString(lines[lines.Count - 1]));
+        }
+
+        byte[] MakeByteArray(params char[] cs) {
+            return cs.Select(c => (byte)c).ToArray();
+        }
+
+        [TestMethod()]
+        public void IsFirstDTest() {
+            Assert.IsTrue(Program.IsFirstD(MakeByteArray('D', 'A', 'B')));
+            Assert.IsTrue(Program.IsFirstD(MakeByteArray('D', 'B', 'A')));
+            Assert.IsFalse(Program.IsFirstD(MakeByteArray('A', 'B', 'D')));
+            Assert.IsFalse(Program.IsFirstD(MakeByteArray('B', 'A', 'D')));
+        }
+
+        [TestMethod()]
+        public void IsFirstMTest() {
+            Assert.IsTrue(Program.IsFirstM(MakeByteArray('M', 'A', 'B')));
+            Assert.IsTrue(Program.IsFirstM(MakeByteArray('M', 'B', 'A')));
+            Assert.IsFalse(Program.IsFirstM(MakeByteArray('A', 'B', 'M')));
+            Assert.IsFalse(Program.IsFirstM(MakeByteArray('B', 'A', 'M')));
+        }
+
+        [TestMethod()]
+        public void FindDateLineIndexTest() {
+            var mail = Encoding.UTF8.GetBytes(MakeSimpleMail());
+            var lines = Program.GetRawLines(mail);
+            Assert.AreEqual(4, Program.FindDateLineIndex(lines));
+
+            lines.RemoveAt(4);
+            Assert.AreEqual(-1, Program.FindDateLineIndex(lines));
+        }
+
+        [TestMethod()]
+        public void FindMessageIdLineIndexTest() {
+            var mail = Encoding.UTF8.GetBytes(MakeSimpleMail());
+            var lines = Program.GetRawLines(mail);
+            Assert.AreEqual(3, Program.FindMessageIdLineIndex(lines));
+
+            lines.RemoveAt(3);
+            Assert.AreEqual(-1, Program.FindMessageIdLineIndex(lines));
+        }
+
+        [TestMethod()]
+        public void ReplaceRawLinesTest() {
+            var mail = Encoding.UTF8.GetBytes(MakeSimpleMail());
+            var lines = Program.GetRawLines(mail);
+
+            var repl_lines_noupdate = Program.ReplaceRawLines(lines, false, false);
+            Assert.AreEqual(lines, repl_lines_noupdate);
+
+            var repl_lines = Program.ReplaceRawLines(lines, true, true);
+            foreach (var i in Enumerable.Range(0, lines.Count).Where(n => n < 3 || n > 4))
+                Assert.AreEqual(lines[i], repl_lines[i]);
+
+            Assert.AreNotEqual(lines[3], repl_lines[3]);
+            Assert.AreNotEqual(lines[4], repl_lines[4]);
+
+            Assert.IsTrue(Encoding.UTF8.GetString(lines[3]).StartsWith("Message-ID: "));
+            Assert.IsTrue(Encoding.UTF8.GetString(repl_lines[3]).StartsWith("Message-ID: "));
+            Assert.IsTrue(Encoding.UTF8.GetString(lines[4]).StartsWith("Date: "));
+            Assert.IsTrue(Encoding.UTF8.GetString(repl_lines[4]).StartsWith("Date: "));
+        }
+
+        [TestMethod()]
+        public void ConcatRawLinesTest() {
+            var mail = Encoding.UTF8.GetBytes(MakeSimpleMail());
+            var lines = Program.GetRawLines(mail);
+
+            var new_mail = Program.ConcatRawLines(lines);
+            Assert.IsTrue(mail.SequenceEqual(new_mail));
+        }
+
+        [TestMethod()]
+        public void ReplaceRawBytesTest() {
+            var mail = Encoding.UTF8.GetBytes(MakeSimpleMail());
+            var repl_mail_noupdate = Program.ReplaceRawBytes(mail, false, false);
+            Assert.AreNotEqual(mail, repl_mail_noupdate);
+            Assert.IsTrue(mail.SequenceEqual(repl_mail_noupdate));
+
+            var repl_mail = Program.ReplaceRawBytes(mail, true, true);
+            Assert.AreNotEqual(mail, repl_mail);
+            Assert.IsFalse(mail.SequenceEqual(repl_mail));
+        }
+
+        [TestMethod()]
+        public void GetSettingsTest() {
+            var path = Path.GetTempFileName();
+            File.WriteAllText(path, Program.MakeJsonSample());
+
+            var settings = Program.GetSettings(path);
+            Assert.AreEqual("172.16.3.151", settings.SmtpHost);
+            Assert.AreEqual(25, settings.SmtpPort);
+            Assert.AreEqual("a001@ah62.example.jp", settings.FromAddress);
+            Assert.IsTrue(new List<string> { "a001@ah62.example.jp", "a002@ah62.example.jp", "a003@ah62.example.jp" }
+                .SequenceEqual(settings.ToAddress));
+            Assert.IsTrue(new List<string> { "test1.eml", "test2.eml", "test3.eml" }
+                .SequenceEqual(settings.EmlFile));
+            Assert.AreEqual(true, settings.UpdateDate);
+            Assert.AreEqual(true, settings.UpdateMessageId);
+            Assert.AreEqual(false, settings.UseParallel);
+        }
+
+        [TestMethod()]
+        public void IsSuccessTest() {
+            Assert.IsTrue(Program.IsSuccess("200 xxx"));
+            Assert.IsTrue(Program.IsSuccess("300 xxx"));
+            Assert.IsFalse(Program.IsSuccess("400 xxx"));
+            Assert.IsFalse(Program.IsSuccess("500 xxx"));
+            Assert.IsFalse(Program.IsSuccess("xxx 200"));
+            Assert.IsFalse(Program.IsSuccess("xxx 300"));
+        }
+
+        public void UseSetStdout(Action block) {
+            var orig_console = Console.Out;
+            try {
+                block();
+            } finally {
+                Console.SetOut(orig_console);
+            }
+        }
+
+        [TestMethod()]
+        public void SendRawBytesTest() {
+            var path = Path.GetTempFileName();
+            var mail = Encoding.UTF8.GetBytes(MakeSimpleMail());
+            File.WriteAllBytes(path, mail);
+
+            UseSetStdout(() => {
+                var str_builder = new StringBuilder();
+                Console.SetOut(new StringWriter(str_builder));
+
+                var mem_stream = new MemoryStream();
+                Program.SendRawBytes(mem_stream, path, false, false);
+                Assert.AreEqual($"send: {path}\r\n", str_builder.ToString());
+                Assert.IsTrue(mail.SequenceEqual(mem_stream.ToArray()));
+            });
+
+            var mem_stream2 = new MemoryStream();
+            Program.SendRawBytes(mem_stream2, path, true, true);
+            Assert.IsFalse(mail.SequenceEqual(mem_stream2.ToArray()));
+        }
+
+        [TestMethod()]
+        public void RecvLineTest() {
+            UseSetStdout(() => {
+                var str_builder = new StringBuilder();
+                Console.SetOut(new StringWriter(str_builder));
+
+                var stream_reader = new StreamReader(new MemoryStream(Encoding.UTF8.GetBytes("250 OK\r\n")));
+                Assert.AreEqual("250 OK", Program.RecvLine(stream_reader));
+                Assert.AreEqual("recv: 250 OK\r\n", str_builder.ToString());
+            });
+
+            var stream_reader2 = new StreamReader(new MemoryStream());
+            Assert.ThrowsException<IOException>(() => Program.RecvLine(stream_reader2));
+
+            var stream_reader3 = new StreamReader(new MemoryStream(Encoding.UTF8.GetBytes("554 Transaction failed\r\n")));
+            Assert.ThrowsException<IOException>(() => Program.RecvLine(stream_reader3));
+        }
+
+        [TestMethod()]
+        public void SendLineTest() {
+            void test(string cmd, string stdout_expected, string writer_expected) {
+                var str_builder = new StringBuilder();
+                Console.SetOut(new StringWriter(str_builder));
+
+                var mem_stream = new MemoryStream();
+                var stream_writer = new StreamWriter(mem_stream);
+
+                Program.SendLine(stream_writer, cmd);
+                Assert.AreEqual(stdout_expected, str_builder.ToString());
+                Assert.AreEqual(writer_expected, Encoding.UTF8.GetString(mem_stream.ToArray()));
+            }
+
+            UseSetStdout(() => {
+                test("EHLO localhost", "send: EHLO localhost\r\n", "EHLO localhost\r\n");
+                test("\r\n.", "send: <CRLF>.\r\n", "\r\n.\r\n");
+            });
+        }
+
+        SendCmd MakeTestSendCmd(string expected) {
+            return cmd => {
+                Assert.AreEqual(expected, cmd);
+                return cmd;
+            };
+        }
+
+        [TestMethod()]
+        public void SendHelloTest() {
+            Program.SendHello(MakeTestSendCmd("EHLO localhost"));
+        }
+
+        [TestMethod()]
+        public void SendFromTest() {
+            Program.SendFrom(MakeTestSendCmd("MAIL FROM: <a001@ah62.example.jp>"), "a001@ah62.example.jp");
+        }
+
+        [TestMethod()]
+        public void SendRcptToTest() {
+            var count = 1;
+            SendCmd test = (string cmd) => {
+                Assert.AreEqual($"RCPT TO: <a00{count}@ah62.example.jp>", cmd);
+                count += 1;
+                return cmd;
+            };
+
+            Program.SendRcptTo(test, new List<string> { "a001@ah62.example.jp", "a002@ah62.example.jp", "a003@ah62.example.jp" });
+        }
+
+        [TestMethod()]
+        public void SendDataTest() {
+            Program.SendData(MakeTestSendCmd("DATA"));
+        }
+
+        [TestMethod()]
+        public void SendCrLfDotTest() {
+            Program.SendCrLfDot(MakeTestSendCmd("\r\n."));
+        }
+
+        [TestMethod()]
+        public void SendQuitTest() {
+            Program.SendQuit(MakeTestSendCmd("QUIT"));
+        }
+
+        [TestMethod()]
+        public void SendRsetTest() {
+            Program.SendRset(MakeTestSendCmd("RSET"));
+        }
+    }
+}
